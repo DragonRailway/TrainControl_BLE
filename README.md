0474ddc1078c3e586c4cd55dcbc9aa3885760075
rambrosteam <protected>
2024-10-09 22:40:17 +0530

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# Loco_BLE_Remote
-Control 3D printed Locomotives with your smartphone using BluetoothLE
+# TrainControl_BLE
+
+This is the official firmware for our DragonRailway Project. **TrainControl_BLE** allows wireless control of a DragonRailway locomotives via Bluetooth (BLE).
+
+## Hardware
+
+### TRACKLINK (recommended)
+
+The **TRACKLINK** board is our custom designed control board for DragonRailway. It comes with the necessary components for your convenience:
+
+- **2 DC Motor Drivers:** Control the train's propulsion motors.
+- **6 LED Drivers:** lights on the train (headlights, tail lights, cablight, walkwaylight, ditchlight etc.)
+- **Voltage sensor:** Detects the battery voltage - for low battery warning
+- **1 MAX98357 Amplifier:** Audio output playing train horn sounds or other effects (Not implemented yet).
+
+### ESP32 or ESP32S3 Dev Boards
+
+This project will still work with a standard dev boards. However, you will need to manually connect external motor drivers. Below are some guidelines for using an ESP32 dev board:
+
+- **DC Motor Control:** Connect external motor drivers (like L298N or TB6612FNG)
+- **LEDs:** Use resistors with LEDs and connect them to available GPIO pins.
+
+## Software
+
+- RemoteXY app - https://remotexy.com/en/download/
+- VSCode with PlatformIO extension
+    - Download VScode https://code.visualstudio.com/
+    - Install PlatformIO https://marketplace.visualstudio.com/items?itemName=platformio.platformio-ide
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
-
+//  Uncomment this line if your model has more than 2 leds
 #define EXTENDED_LED_CONTROL
 
-//  https://github.com/bxparks/AceRoutine
-#include <AceRoutine.h>  // AceRoutine
+#include <Arduino.h>
+#include <pwmWrite.h>  //  https://github.com/Dlloydev/ESP32-ESP32S2-AnalogWrite
+Pwm pwm = Pwm();
+#include <AceRoutine.h>  //  https://github.com/bxparks/AceRoutine
 using namespace ace_routine;
-
-//  https://github.com/madhephaestus/ESP32Servo
 
 //  https://github.com/RemoteXY/RemoteXY-Arduino-library
 #define REMOTEXY_MODE__ESP32CORE_BLE
@@ -21,6 +21,29 @@ using namespace ace_routine;
 
 // RemoteXY GUI configuration
 #pragma pack(push, 1)
+
+int16_t speed = 0;
+int8_t dir = 1;
+int8_t minPower = 12;  //  in percentage
+
+float vBat = 0.00;
+uint8_t lowBatWarning = 0;
+
+bool lightState[5] = { 0 };
+enum LedIndex {
+  HEADLIGHT = 0,  //  L1
+  TAILLIGHT = 1,  //  L2
+  CABLIGHT = 2,   //  L3
+  WALKLIGHT = 3,  //  L4
+  DITCHLIGHT = 4  //  L5,L6
+};
+
+// Variables to keep track of led brightness
+int16_t headlightBrightness = 0;
+int16_t taillightBrightness = 0;
+int16_t cablightBrightness = 0;
+int16_t walklightBrightness = 0;
+int16_t ditchlightBrightness = 0;
 
 //////////////////////////////////////////////
 //     RemoteXY include configuration       //
@@ -86,39 +109,30 @@ struct {
 //           END RemoteXY include          //
 /////////////////////////////////////////////
 
-int16_t speed = 0;
-int8_t dir = 1;
-
-float voltage = 0.00;
-
-bool headlight = 0;   //  L1
-bool taillight = 0;   //  L2
-bool cablight = 0;    //  L3
-bool walklight = 0;   //  L4
-bool ditchlight = 0;  //  L5,L6
-
-int16_t headlightBrightness = 0;
-int16_t taillightBrightness = 0;
-int16_t cablightBrightness = 0;
-int16_t walklightBrightness = 0;
-int16_t ditchlightBrightness = 0;
-
-int8_t led_select = 0;
-
-
 int16_t fade(uint8_t pinNumber, bool ledState, int16_t ledBrightness) {
-  if (ledState == 1 && ledBrightness < 255) {
-    ledBrightness = ledBrightness + 1;
-  } else if (ledState == 0 && ledBrightness > 0) {
-    ledBrightness = ledBrightness - 1;
+  int16_t prevBrightness = ledBrightness;
+  if (ledState == 1 && ledBrightness < 255) {       // if led is on and brightness is less than max
+    ledBrightness = ledBrightness + 1;              // increase brightness by 1
+  } else if (ledState == 0 && ledBrightness > 0) {  // if led is off and brightness is greater than 0
+    ledBrightness = ledBrightness - 1;              // decrease brightness by 1
   }
-  ledBrightness = constrain(ledBrightness, 0, 255);
-  analogWrite(pinNumber, ledBrightness);
-  return ledBrightness;
+  ledBrightness = constrain(ledBrightness, 0, 255);  // limit brightness between 0 and 255
+
+  if (ledBrightness >= 255 || ledBrightness <= 0) {  // if brightness did not change
+    if (prevBrightness != ledBrightness) {
+      pwm.detach(pinNumber);  // this frees up a ledc channel
+      pinMode(pinNumber, OUTPUT);
+    }
+
+    digitalWrite(pinNumber, ledState);
+  } else {
+    pwm.write(pinNumber, ledBrightness, LED_FREQ, LED_FREQ);  // write brightness to the led pin
+  }
+  return ledBrightness;  // return the new brightness value
 }
 
 int16_t alternateFade(uint8_t pinNumber1, uint8_t pinNumber2, bool ledState, int16_t ledBrightness) {
-  static int8_t alternateBrightness = 2;
+  static int8_t alternateBrightness = 2;  // direction, strength of brightness change
   if (ledState == 1) {
     if (ledBrightness >= 255) {
       alternateBrightness = alternateBrightness * -1;
@@ -127,20 +141,48 @@ int16_t alternateFade(uint8_t pinNumber1, uint8_t pinNumber2, bool ledState, int
       alternateBrightness = alternateBrightness * -1;
     }
     ledBrightness = ledBrightness + alternateBrightness;
-    ledBrightness = constrain(ledBrightness, 0, 255);
-    analogWrite(pinNumber1, ledBrightness);
-    analogWrite(pinNumber2, 255 - ledBrightness);
+    ledBrightness = constrain(ledBrightness, 0, 255);                // limit pwm duty range
+    pwm.write(pinNumber1, ledBrightness, LED_FREQ, LED_FREQ);        // write brightness to pin 1
+    pwm.write(pinNumber2, 255 - ledBrightness, LED_FREQ, LED_FREQ);  // write inverse brightness to pin 2
 
   } else {
-    analogWrite(pinNumber1, 0);
-    analogWrite(pinNumber2, 0);
+    pwm.detach(pinNumber1);       // detach pin 1 from Pwm
+    pwm.detach(pinNumber2);       // detach pin 2 from Pwm
+    pinMode(pinNumber1, OUTPUT);  // set pin 1 to output
+    pinMode(pinNumber2, OUTPUT);  // set pin 2 to output
+    digitalWrite(pinNumber1, 0);  // turn off pin 1
+    digitalWrite(pinNumber2, 0);  // turn off pin 2
   }
 
-  return ledBrightness;
+  return ledBrightness;  // return new brightness value
+}
+
+void runMotor(uint8_t pwmPin1, uint8_t pwmPin2, int16_t speed, bool direction) {
+  uint16_t minPwmDuty = minPower * 255 / 100;
+
+  if (speed > 0) {
+    speed = map(speed, 0, 100, minPwmDuty, 255);
+    digitalWrite(DRV_EN, HIGH);
+    switch (direction) {
+      case 0:
+        pwm.write(pwmPin1, 0);
+        pwm.write(pwmPin2, speed);
+        break;
+      case 1:
+        pwm.write(pwmPin1, speed);
+        pwm.write(pwmPin2, 0);
+        break;
+    }
+  } else {
+    digitalWrite(DRV_EN, LOW);
+    pwm.write(pwmPin1, 0);
+    pwm.write(pwmPin2, 0);
+  }
 }
 void setup() {
   RemoteXY_Init();
 
+  //  LED pins
   pinMode(L1, OUTPUT);
   pinMode(L2, OUTPUT);
   pinMode(L3, OUTPUT);
@@ -149,15 +191,22 @@ void setup() {
   pinMode(L6, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
 
-  pinMode(DRV_MA1, OUTPUT);
-  pinMode(DRV_MA2, OUTPUT);
-  pinMode(DRV_MB1, OUTPUT);
-  pinMode(DRV_MB2, OUTPUT);
+  //  H bridge pins
+  pwm.attach(DRV_MA1, 0);
+  pwm.attach(DRV_MA2, 1);
+  pwm.attach(DRV_MB1, 0);
+  pwm.attach(DRV_MB2, 1);
+  pwm.setFrequency(DRV_MA1, DRV_FREQ);
+  pwm.setFrequency(DRV_MA2, DRV_FREQ);
+  pwm.setResolution(DRV_MA1, DRV_RES);
+  pwm.setResolution(DRV_MA2, DRV_RES);
   pinMode(DRV_EN, OUTPUT);
+  digitalWrite(DRV_EN, LOW);
 
+  //  Audio amplifier pins - not implemented yet
   pinMode(SD_MODE, OUTPUT);
   digitalWrite(SD_MODE, LOW);
-
+  //  Servo Pins
   pinMode(S1, OUTPUT);
   pinMode(S2, OUTPUT);
 }
@@ -165,45 +214,16 @@ void setup() {
 #ifdef EXTENDED_LED_CONTROL
 COROUTINE(ledInput) {
   COROUTINE_LOOP() {
-
+    static int8_t led_select = 0;
     switch (RemoteXY.led_id) {
       case 0:
         if (led_select != RemoteXY.led_id) {
           led_select = RemoteXY.led_id;
-          RemoteXY.toggle = headlight;
+          RemoteXY.toggle = lightState[led_select];
         }
-        headlight = RemoteXY.toggle;
-        break;
-      case 1:
-        if (led_select != RemoteXY.led_id) {
-          led_select = RemoteXY.led_id;
-          RemoteXY.toggle = taillight;
-        }
-        taillight = RemoteXY.toggle;
-        break;
-      case 2:
-        if (led_select != RemoteXY.led_id) {
-          led_select = RemoteXY.led_id;
-          RemoteXY.toggle = cablight;
-        }
-        cablight = RemoteXY.toggle;
-        break;
-      case 3:
-        if (led_select != RemoteXY.led_id) {
-          led_select = RemoteXY.led_id;
-          RemoteXY.toggle = walklight;
-        }
-        walklight = RemoteXY.toggle;
-        break;
-      case 4:
-        if (led_select != RemoteXY.led_id) {
-          led_select = RemoteXY.led_id;
-          RemoteXY.toggle = ditchlight;
-        }
-        ditchlight = RemoteXY.toggle;
+        lightState[led_select] = RemoteXY.toggle;
         break;
     }
-
     COROUTINE_DELAY(50);
   }
 }
@@ -211,8 +231,8 @@ COROUTINE(ledInput) {
 #else
 COROUTINE(ledInput) {
   COROUTINE_LOOP() {
-    headlight = RemoteXY.toggle1;
-    taillight = RemoteXY.toggle1;
+    lightState[HEADLIGHT] = RemoteXY.toggle;
+    lightState[TAILLIGHT] = RemoteXY.toggle1;
     COROUTINE_DELAY(100);
   }
 }
@@ -220,39 +240,45 @@ COROUTINE(ledInput) {
 
 COROUTINE(ledOutput) {
   COROUTINE_LOOP() {
+    headlightBrightness = fade(LED_BUILTIN, lightState[HEADLIGHT], headlightBrightness);
 
-    headlightBrightness = fade(L1, headlight, headlightBrightness);
-    headlightBrightness = fade(L2, headlight, headlightBrightness);
-    taillightBrightness = fade(L2, taillight, taillightBrightness);
-    cablightBrightness = fade(L3, cablight, cablightBrightness);
-    walklightBrightness = fade(L4, walklight, walklightBrightness);
-    ditchlightBrightness = alternateFade(L5, L6, ditchlight, ditchlightBrightness);
+    headlightBrightness = fade(L1, lightState[HEADLIGHT], headlightBrightness);
+    taillightBrightness = fade(L2, lightState[TAILLIGHT], taillightBrightness);
+    cablightBrightness = fade(L3, lightState[CABLIGHT], cablightBrightness);
+    walklightBrightness = fade(L4, lightState[WALKLIGHT], walklightBrightness);
+    ditchlightBrightness = alternateFade(L5, L6, lightState[DITCHLIGHT], ditchlightBrightness);
     COROUTINE_DELAY(10);
   }
 }
 
 COROUTINE(motorOutput) {
   COROUTINE_LOOP() {
-
+    runMotor(DRV_MA1, DRV_MA2, RemoteXY.speed, RemoteXY.dir);
+    //runMotor(DRV_MB1, DRV_MB2, RemoteXY.speed, RemoteXY.dir);
     COROUTINE_DELAY(10);
   }
 }
 
-COROUTINE(blink) {
+COROUTINE(health) {
   COROUTINE_LOOP() {
-    digitalWrite(LED_BUILTIN, HIGH);
-    COROUTINE_DELAY(500);
-    digitalWrite(LED_BUILTIN, LOW);
-    COROUTINE_DELAY(500);
+    if (RemoteXY.connect_flag == 0) {
+      digitalWrite(DRV_EN, LOW);
+      RemoteXY.speed = 0;
+      RemoteXY.toggle = 0;
+      lightState[HEADLIGHT] = !lightState[HEADLIGHT];
+      lightState[5] = 1;
+    }
+    vBat = analogRead(VSENS) * VSCALE / 1000;
+    COROUTINE_DELAY(500);  //  Check health 2 times/second
   }
 }
-
 
 void loop() {
   RemoteXY_Handler();
 
   ledInput.runCoroutine();
-  ledOutput.runCoroutine();
+  health.runCoroutine();
+
   motorOutput.runCoroutine();
-  blink.runCoroutine();
+  ledOutput.runCoroutine();
 }
