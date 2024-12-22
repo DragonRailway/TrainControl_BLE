af82caac5406f67533fca6317e389fa33b42dcba
rambrosteam <protected>
2024-12-22 13:46:59 +0530

    fix pin error for ESP32 dev board

--- custom_pins.h
+++ custom_pins.h
@@ -6,18 +6,18 @@
 #define DRV_MA1 12 //  PWM1 pin
 #define DRV_MA2 13 //  PWM2 pin
 // Second Motor driver pins, only required if you're using DUAL_MOTOR_DRIVER option
-#define DRV_MB1 14 //  PWM1 pin
-#define DRV_MB2 15 //  PWM2 pin
+// #define DRV_MB1 14 //  PWM1 pin
+// #define DRV_MB2 15 //  PWM2 pin
 //  Driver enable pin, assign dummy pin if your driver doesn't have enable pin
-#define DRV_EN 11
+#define DRV_EN 14
 
 // LED pins
 #define L1 5
-#define L2 6
-#define L3 7
-#define L4 8
-#define L5 9
-#define L6 10
+#define L2 15
+#define L3 17
+#define L4 18
+#define L5 19
+#define L6 21
 
 #define VSENS 4
 
