3e783a677282cae1299aa8c598e5c6ea9a37f560
rambrosteam <protected>
2025-02-06 00:37:44 +0530

    adjust pins to remove SPI boot error

--- custom_pins.h
+++ custom_pins.h
@@ -3,21 +3,21 @@
 #ifdef DIY_BOARD
 
 // Motor driver pins
-#define DRV_MA1 12 //  PWM1 pin
-#define DRV_MA2 13 //  PWM2 pin
+#define DRV_MA1 32 //  PWM1 pin
+#define DRV_MA2 33 //  PWM2 pin
 // Second Motor driver pins, only required if you're using DUAL_MOTOR_DRIVER option
 // #define DRV_MB1 14 //  PWM1 pin
 // #define DRV_MB2 15 //  PWM2 pin
 //  Driver enable pin, assign dummy pin if your driver doesn't have enable pin
-#define DRV_EN 14
+#define DRV_EN 23
 
 // LED pins
-#define L1 5
-#define L2 15
-#define L3 17
-#define L4 18
-#define L5 19
-#define L6 21
+#define L1 12
+#define L2 14
+#define L3 27
+#define L4 26
+#define L5 25
+#define L6 23
 
 #define VSENS 4
 #define VSCALE 4.2
